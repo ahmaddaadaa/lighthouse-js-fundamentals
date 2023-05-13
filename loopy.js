018776b354e9aebda6b0d39556b66d3e06ebb8d0
ahmaddaadaa <ahmadd@uvic.ca>
2023-05-13 16:19:39 -0700

    Is Even Function

---
+++ loopy.js
@@ -0,0 +1,11 @@
+var num=100;
+for (var i=0; i<=100; i++)
+{
+if(num%3==0 && num%4==0){console.log("LoopyLighthouse");}
+else if(num%3==0){console.log("Loopy");}
+else if(num%4==0){console.log("Lighthouse");}
+else {
+console.log(num);
+}
+num++;
+}
